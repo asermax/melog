5f672e6b01c38a7f7fcfd63927a9dcc00f82532a
Agustin Carrasco <asermax@gmail.com>
2013-07-08 23:16:14 -0300

    models: added initial Log model

---
+++ src/log/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from log import models
+
+
+admin.site.register(models.Log)
--- src/log/models.py
+++ src/log/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Log(models.Model):
+    user = models.ForeignKey('auth.User')
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date',)
+
+    def __unicode__(self):
+        username = self.user.username
+        text = self.text[:20] + '...' if len(self.text) > 20 else self.text
+        date = self.date.strftime('%d/%m/%Y %H:%M')
+
+        return '{0} - {1}: {2}'.format(date, username, text)
