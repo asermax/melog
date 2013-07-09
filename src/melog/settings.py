46da5f014c14034c0aa51ff89d09851f00bcc0f6
Agustin Carrasco <asermax@gmail.com>
2013-07-08 23:11:56 -0300

    settings & urls: added admin

--- src/melog/settings.py
+++ src/melog/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'mediagenerator',
     'south',
     'log',
