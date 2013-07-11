c0fc5cb586686400ac82542abd0f36a50bef8571
Agustin Carrasco <asermax@gmail.com>
2013-07-11 00:32:13 -0300

    api: agregadas algunas clases base para facilitar el manejo de modelos por usuario

--- src/log/api.py
+++ src/log/api.py
@@ -4,6 +4,54 @@ from tastypie import resources, authentication, authorization, fields
 from . import models
 
 
+class PerUserAuthorization(authorization.Authorization):
+    def read_list(self, object_list, bundle):
+        return object_list.filter(user=bundle.request.cliente.user)
+
+    def read_detail(self, object_list, bundle):
+        # Is the requested object owned by the user?
+        return bundle.obj.user == bundle.request.user
+
+    def create_list(self, object_list, bundle):
+        # Assuming their auto-assigned to ``user``.
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def update_list(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def update_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+    def delete_list(self, object_list, bundle):
+        allowed = []
+
+        # Since they may not all be saved, iterate over them.
+        for obj in object_list:
+            if obj.user == bundle.request.user:
+                allowed.append(obj)
+
+        return allowed
+
+    def delete_detail(self, object_list, bundle):
+        return bundle.obj.user == bundle.request.user
+
+
+class PerUserResource(resources.ModelResource):
+    def obj_create(self, bundle, **kwargs):
+        return super(PerUserResource, self).obj_create(
+            bundle, user=bundle.request.user)
+
+
 class UserResource(resources.ModelResource):
     class Meta:
         queryset = auth.models.User.objects.all()
@@ -14,7 +62,7 @@ class UserResource(resources.ModelResource):
         authorization = authorization.ReadOnlyAuthorization()
 
 
-class LogResource(resources.ModelResource):
+class LogResource(PerUserResource):
     user = fields.ForeignKey(UserResource, 'user')
 
     class Meta:
@@ -22,3 +70,4 @@ class LogResource(resources.ModelResource):
         resource_name = 'log'
         always_return_data = True
         authentication = authentication.SessionAuthentication()
+        authorization = PerUserAuthorization()
